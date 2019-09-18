7f1b528902e7a18356175e0f049c679bcc1c429d
malik <malik.h@webdevpro.net>
2019-09-18 11:32:37 +0200

    Ajouter une balise html personnalisée dans la vue du module root

--- src/app/premier.component.ts
+++ src/app/premier.component.ts
@@ -11,8 +11,11 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    selector: "premier",
-    template : "<h1>premier</h1>"
+    selector: "premier", 
+    // premier => vous avez créé une nouvelle balise html
+    // <premier></premier>
+    // rappel <app-root></app-root>
+    template : "<h1>premier</h1>",
 })
 export class PremierComponent{
 
