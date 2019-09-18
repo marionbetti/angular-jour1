3a796469dd67daf8dbc7af0437e6099450f036f7
malik <malik.h@webdevpro.net>
2019-09-18 11:22:03 +0200

    créer le fichier composant premier

---
+++ src/app/premier.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+// si vous avez à créer un composant avec un nom composé
+
+// le nom du fichier menu-top.component.ts
+// pas de majuscule pas de _ tiret bas = underscore 
+
+// il faut les mettre dans le dossier /src/app
+
+// décorer la class via une fonction décoratrice
+
+
+@Component({
+    selector: "premier",
+    template : "<h1>premier</h1>"
+})
+export class PremierComponent{
+
+}
+
+// si nom composé pour le composant
+//export class MenuTopComponent{
+//
+//}
