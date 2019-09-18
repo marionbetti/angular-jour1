87a961f4dfb237eab213d296f343f79fb783ac5c
malik <malik.h@webdevpro.net>
2019-09-18 11:26:30 +0200

    enregitrer notre module premier dans app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PremierComponent } from './premier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremierComponent
   ],
   imports: [
     BrowserModule
